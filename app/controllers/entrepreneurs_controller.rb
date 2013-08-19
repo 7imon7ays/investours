a0c023077762f984b2812d2bb2c771aacb8f610c
Simon Chaffetz <simis1000@gmail.com>
2013-08-18 22:13:56 -0700

    Create welcome view and entrepreneurs views.

---
+++ app/controllers/entrepreneurs_controller.rb
@@ -0,0 +1,49 @@
+class EntrepreneursController < ApplicationController
+  
+  def index
+    @entrepreneurs = Entrepreneur.all
+  end
+  
+  def show
+    @entrepreneur = Entrepreneur.find(params[:id])
+  end
+  
+  def new
+    @entrepreneur = Entrepreneur.new
+  end
+  
+  def create
+    @entrepreneur = Entrepreneur.new(params[:entrepreneur])
+    
+    if @entrepreneur.save
+      redirect_to entrepreneur_url(@entrepreneur)
+      flash[:notice] = "#{@entrepreneur.first_name} #{@entrepreneur.last_name} successfully added."
+    else
+      flash.now[:error] = @entrepreneur.errors.full_messages
+      render action: "new"
+    end
+  end
+  
+  def edit
+    @entrepreneur = Entrepreneur.find(params[:id])
+  end
+  
+  def update
+    @entrepreneur = Entrepreneur.update_attributes(params[:entrepreneur])
+    
+    if @entrepreneur.save
+      redirect_to entrepreneur_url(@entrepreneur)
+      flash[:notice] = "#{@entrepreneur.first_name} #{@entrepreneur.last_name} successfully updated."
+    else
+      flash.now[:error] = @entrepreneur.errors.full_messages
+      render action: "edit"
+    end
+  end
+  
+  def destroy
+    @entrepreneur = Entrepreneur.find(params[:id])
+    @entrepreneur.destroy
+    redirect_to entrepreneurs_url
+  end
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Investours::Application.routes.draw do
   devise_for :users
+  root to: "visitors#welcome"
+  resource :dashboard, only: [:show]
+  resources :entrepreneurs
 end
