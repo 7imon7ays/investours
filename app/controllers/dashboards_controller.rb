17473a88e4a251cb9f5f53828062599b8f1af935
Simon Chaffetz <simis1000@gmail.com>
2013-09-24 21:42:33 -0700

    Build basic project updates views, minus new update form.

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -3,7 +3,6 @@ class DashboardsController < ApplicationController
   before_filter :authenticate_user!
   
   def show
-    @user = current_user
     @projects = @user.backed_projects
   end
   
--- app/models/repayment.rb
+++
@@ -1,5 +0,0 @@
-class Repayment < ActiveRecord::Base
-  # attr_accessible :title, :body
-  
-  belongs_to :loan
-end
